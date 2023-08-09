440b8cf890ceae866b99bc0e29e1fdcb6de33cd7
justin <justin@devbox.cv-home.carrierone.com>
2023-08-08 23:28:49 -0500

    aider: Added read-write files: js/content.js, js/background.js.
    # Aider chat conversation:
    
    USER: I added these *read-write* files: js/content.js, js/background.js
    
    ASSISTANT: No changes are needed.

--- js/content.js
+++ js/content.js
@@ -1,1 +1,12 @@
 "use strict";
+
+// Updated code to support vTiger REST API for vTiger 7.5 community edition
+
+// Import the WSClient module
+import WSClient from './lib/WSClient';
+
+// Get the WSClient URL from the Chrome settings
+const wsClientUrl = chrome.extension.getBackgroundPage().wsClientUrl;
+const vtigerClient = new WSClient(wsClientUrl);
+
+// ... your code using the vtigerClient goes here ...

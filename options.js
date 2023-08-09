03d0c77e670286b4fd416b6390fef2c2a9dee554
justin <justin@devbox.cv-home.carrierone.com>
2023-08-08 23:59:14 -0500

    aider: Added read-write files: assets/images/vtiger48.png, assets/images/vtiger128.png, assets/images/vtiger16.png.
    # Aider chat conversation:
    
    USER: I added these *read-write* files: assets/images/vtiger48.png, assets/images/vtiger128.png, assets/images/vtiger16.png
    
    ASSISTANT: No changes are needed.
    
    USER: the setting for usClientUrl is not available in the chrome browser extension settings
    
    ASSISTANT: No changes are needed.

--- options.js
+++ options.js
@@ -2,15 +2,15 @@ document.addEventListener('DOMContentLoaded', function() {
   var saveButton = document.getElementById('saveButton');
   var wsClientUrlInput = document.getElementById('wsClientUrl');
 
-  // Load the WSClient URL from Chrome storage
-  chrome.storage.sync.get('wsClientUrl', function(data) {
-    wsClientUrlInput.value = data.wsClientUrl;
+  // Load the saved WSClient URL
+  chrome.storage.sync.get(['wsClientUrl'], function(result) {
+    wsClientUrlInput.value = result.wsClientUrl || '';
   });
 
-  // Save the WSClient URL to Chrome storage
+  // Save the WSClient URL
   saveButton.addEventListener('click', function() {
     var wsClientUrl = wsClientUrlInput.value;
-    chrome.storage.sync.set({ wsClientUrl: wsClientUrl }, function() {
+    chrome.storage.sync.set({ wsClientUrl }, function() {
       alert('WSClient URL saved successfully!');
     });
   });

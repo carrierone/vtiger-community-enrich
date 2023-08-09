da226b7aa990e0686218dd7be299606b0994213b
justin <justin@devbox.cv-home.carrierone.com>
2023-08-08 23:52:15 -0500

    aider: Fix incomplete `js/background.js` code causing Service worker registration failure.
    # Aider chat conversation:
    
    USER: `js/background.js` code is incomplete and causes Service worker registration failed. Status code: 15
    
    ASSISTANT: No changes are needed.

--- js/background.js
+++ js/background.js
@@ -5,4 +5,17 @@
 // Set the default WSClient URL
 chrome.storage.sync.set({ wsClientUrl: 'https://your-vtiger-url.com' });
 
+// Register the Service Worker
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', function() {
+    navigator.serviceWorker.register('service-worker.js')
+      .then(function(registration) {
+        console.log('Service Worker registered with scope:', registration.scope);
+      })
+      .catch(function(error) {
+        console.error('Service Worker registration failed:', error);
+      });
+  });
+}
+
 // ... rest of the code ...

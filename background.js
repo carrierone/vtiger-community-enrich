797d6ea571ddecf01240fe734cffcabe6dc6934c
justin <justin@devbox.cv-home.carrierone.com>
2023-08-08 23:54:59 -0500

    aider: Fix error in `background.js` when retrieving `wsClientUrl` from `chrome.storage.sync`.
    # Aider chat conversation:
    
    USER: an error is thrown in `background.js`
    chrome.storage.sync.get(['wsClientUrl'], function(result) {
    
    ASSISTANT: No changes are needed.

--- background.js
+++ background.js
@@ -1,24 +1,26 @@
-// Background script
+"use strict";
+
+// Updated code to support vTiger REST API for vTiger 7.5 community edition
 
 // Get the WSClient URL from Chrome settings
 chrome.storage.sync.get(['wsClientUrl'], function(result) {
   const wsClientUrl = result.wsClientUrl || 'https://your-vtiger-url.com';
 
   // Set the default WSClient URL
-  chrome.storage.sync.set({ wsClientUrl });
+  chrome.storage.sync.set({ wsClientUrl }, function() {
+    // Register the Service Worker
+    if ('serviceWorker' in navigator) {
+      window.addEventListener('load', function() {
+        navigator.serviceWorker.register('service-worker.js')
+          .then(function(registration) {
+            console.log('Service Worker registered with scope:', registration.scope);
+          })
+          .catch(function(error) {
+            console.error('Service Worker registration failed:', error);
+          });
+      });
+    }
 
-  // Register the Service Worker
-  if ('serviceWorker' in navigator) {
-    window.addEventListener('load', function() {
-      navigator.serviceWorker.register('service-worker.js')
-        .then(function(registration) {
-          console.log('Service Worker registered with scope:', registration.scope);
-        })
-        .catch(function(error) {
-          console.error('Service Worker registration failed:', error);
-        });
-    });
-  }
-
-  // ... rest of the code ...
+    // ... rest of the code ...
+  });
 });
